3d1847945a4686094df712ec19bc52029de1df1e
Matthew <sowhoomamode@gmail.com>
2024-04-22 13:51:45 +0100

    Add project files.

---
+++ Simple_Calculator/Calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    public class Calc
+    {
+        public int Add(int x, int y)
+        {
+            int answer= x + y;
+            Console.Write("Answer = ");
+            Console.Write(answer);
+            return answer;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            int answer = x - y;
+            Console.Write("Answer = ");
+            Console.Write(answer);
+            return answer;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            int answer = x * y;
+            Console.Write("Answer = ");
+            Console.Write(answer);
+            return answer;
+        }
+
+        public int Divide(int x, int y)
+        {
+            int answer = x / y;
+            Console.Write("Answer = ");
+            Console.Write(answer);
+            return answer;
+        }
+
+        public double CalculateAverage(double[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                throw new ArgumentException("Array must contain at least one element");
+            }
+
+            double sum = 0;
+            foreach (double num in numbers)
+            {
+                sum += num;
+            }
+
+            return sum / numbers.Length;
+        }
+
+    }
+}
---
+++ Simple_Calculator/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            options:
+            Console.WriteLine("Select an operation \n 1.Add \n 2.Subtract \n 3.Multiply \n 4.Divide");
+            string option= Console.ReadLine();
+            Calc calc = new Calc();
+            if (option == "1")
+            {
+                
+                Console.WriteLine("Input two numbers");
+                Console.WriteLine("First Number:");
+                first:
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                {
+                    Console.WriteLine("This is not a number");
+                    goto first;
+                }
+                Console.WriteLine("Second Number:");
+                second:
+                if (!int.TryParse(Console.ReadLine(), out int y))
+                {
+                    Console.WriteLine("This is not a number");
+                    goto second;
+                }
+                calc.Add(x, y);
+            }
+
+            else if (option == "2")
+            {
+                Console.WriteLine("Input two numbers");
+                Console.WriteLine("First Number:");
+            first:
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                {
+                    Console.WriteLine("This is not a number");
+                    goto first;
+                }
+                Console.WriteLine("Second Number:");
+            second:
+                if (!int.TryParse(Console.ReadLine(), out int y))
+                {
+                    Console.WriteLine("This is not a number");
+                    goto second;
+                }
+                calc.Subtract(x, y);
+            }
+
+            else if (option == "3")
+            {
+                Console.WriteLine("Input two numbers");
+                Console.WriteLine("First Number:");
+            first:
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                {
+                    Console.WriteLine("This is not a number");
+                    goto first;
+                }
+                Console.WriteLine("Second Number:");
+            second:
+                if (!int.TryParse(Console.ReadLine(), out int y))
+                {
+                    Console.WriteLine("This is not a number");
+                    goto second;
+                }
+                calc.Multiply(x, y);
+            }
+            else if (option == "4")
+            {
+                Console.WriteLine("Input two numbers");
+                Console.WriteLine("First Number:");
+            first:
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                {
+                    Console.WriteLine("This is not a number");
+                    goto first;
+                }
+                Console.WriteLine("Second Number:");
+            second:
+                if (!int.TryParse(Console.ReadLine(), out int y))
+                {
+                    Console.WriteLine("This is not a number");
+                    goto second;
+                }
+                calc.Divide(x, y);
+            }
+
+            else if(option == "5")
+            {
+                
+                Console.WriteLine("Enter numbers separated by commas:");
+                string input = Console.ReadLine();
+
+                string[] numberStrings = input.Split(',');
+                double[] numbers = new double[numberStrings.Length];
+
+                for (int i = 0; i < numberStrings.Length; i++)
+                {
+                    if (!double.TryParse(numberStrings[i], out numbers[i]))
+                    {
+                        Console.WriteLine($"Invalid input: {numberStrings[i]} is not a valid number.");
+                        return;
+                    }
+                }
+
+                double average = calc.CalculateAverage(numbers);
+                Console.WriteLine($"The average is: {average}");
+            }
+
+           
+            else
+            {
+                Console.WriteLine("Input a valid option");
+                goto options;
+            }
+        }
+            
+    }
+}
